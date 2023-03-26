4ca833aba3e9d141e4f783b0f8626b87f1989a57
nathanreynolds-bb <101427661+nathanreynolds-bb@users.noreply.github.com>
2023-03-26 18:06:30 -0500

    finish themeSwitch functionality and touch-up styles

--- themeSwitch.js
+++ themeSwitch.js
@@ -2,13 +2,56 @@
 const toggleButton = document.getElementById("themeSwitch");
 
 //Define callback function for toggle button
-function swapTheme(e) {
-	if (e.target.style.color == 'white') {
-		e.target.style.color = 'black';
+function swapColors(color) {
+	const navLinks = document.getElementsByClassName("nav-link");
+	const sections = document.getElementsByTagName("section");
+	if (color == "rgb(255, 255, 255)" || color == "white") {
+		// handle nav bar
+		for (const link of navLinks) {
+			link.style.color = "black";
+		}
+		document.getElementsByTagName("nav")[0].style.backgroundColor = "#9f9f9f";
+
+		// handle sections
+		for (section of sections) {
+			section.style.color = "white";
+			section.style.backgroundColor = "#1c1c1c"
+		}
+
+		// handle contact links
+		if (document.title == "Contact") {
+			console.log('is Contact')
+			for (link of document.getElementsByClassName("contact-link")) {
+				console.log(link)
+				link.style.color = "#aaaaaa";
+			}
+		}
 	} else {
-		e.target.style.color = 'white';
+		// handle nav bar
+		for (const link of navLinks) {
+			link.style.color = "white";
+		}
+		document.getElementsByTagName("nav")[0].style.backgroundColor = "#4c4c4c";
+
+		// handle sections
+		for (section of sections) {
+			section.style.color = "";
+			section.style.backgroundColor = ""
+		}
+
+		// handle contact links
+		if (document.title == "Contact") {
+			console.log('is Contact')
+			for (link of document.getElementsByClassName("contact-link")) {
+				console.log(link)
+				link.style.color = "";
+			}
+		}
 	}
+}
+function swapTheme(e) {
+	swapColors(getComputedStyle(toggleButton).getPropertyValue("color"));
 }
 
 //Add an event listener to the toggle button
-toggleButton.addEventListener('click', swapTheme)
+toggleButton.addEventListener("click", swapTheme);
